07f0c9abfb1703bc544404d3b61859a060470e0a
Jacob Janisz <jacob@jojko.tech>
2026-03-26 17:36:08 +0000

    build: lostpixel config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -4,6 +4,6 @@ export const config: CustomProjectConfig = {
   histoireShots: {
     histoireUrl: './.histoire/dist',
   },
-  generateOnly: true,
-  failOnDifference: true
+  lostPixelProjectId: 'cmn3g8usg59szo05sd9dnn0gz',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
 }
